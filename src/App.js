6773c78f80c669bae7eb33aef1ff1d619c9529dc
alvalenda <banysan@msn.com>
2022-09-14 20:55:12 -0300

    feat: TodoMetadata

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css'
 import { Header } from './components/Header'
 import { TodoForm } from './components/TodoForm'
 import { TodoList } from './components/TodoList'
+import { TodoMetadata } from './components/TodoMetadata'
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <TodoForm filter={true} />
+      <TodoMetadata />
       <TodoList />
     </div>
   )
