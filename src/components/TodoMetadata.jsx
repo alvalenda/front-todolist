491676950ffd410277ba1e055ede9e3b91457c8b
alvalenda <banysan@msn.com>
2022-09-15 09:36:58 -0300

    fix: calcs inline

--- src/components/TodoMetadata.jsx
+++ src/components/TodoMetadata.jsx
@@ -2,22 +2,16 @@ import './TodoMetadata.css'
 import { useEffect } from 'react'
 
 export const TodoMetadata = ({ todoList }) => {
-  let totalTodos, remainingTodos
-
-  const setMetadata = () => {
-    totalTodos = todoList.length
-    remainingTodos = todoList.reduce((acc, item) => {
-      if (item.completed) return acc
-      else return acc + 1
-    }, 0)
-    console.log(totalTodos, remainingTodos)
-  }
-  setMetadata()
-
   return (
     <div className='TodoMetadata'>
-      <span>{totalTodos ? totalTodos : 0} todos</span>
-      <span>{remainingTodos ? remainingTodos : 0} remaining</span>
+      <span>{todoList.length ? todoList.length : 0} todos</span>
+      <span>
+        {todoList.reduce((acc, item) => {
+          if (item.completed) return acc
+          else return acc + 1
+        }, 0)}{' '}
+        remaining
+      </span>
     </div>
   )
 }
