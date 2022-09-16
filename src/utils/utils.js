f0e21038100b2daf9e9a7344a592c3bcc5ed5141
alvalenda <banysan@msn.com>
2022-09-16 11:13:23 -0300

    feat: motionItem, motionExit

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -19,3 +19,44 @@ const listSortedById = (list) => {
 export const sortedTodoList = (todoList) => {
   todoList.sort((a, b) => a.completed - b.completed || a.id - b.id)
 }
+
+/////////////////////////////// MOTION
+export const motionItem = {
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      stiffness: 100,
+      x: { duration: 0.8, easing: 'easeOutQuart' },
+      y: { duration: 0.8, easing: 'easeOutQuart' },
+      opacity: { duration: 1.5, easing: 'easeOutQuart' },
+      default: { ease: 'linear' },
+    },
+  },
+  completed: {
+    x: 0,
+    y: 0,
+    opacity: 0.4,
+    transition: {
+      type: 'spring',
+      stiffness: 100,
+      x: { duration: 0.8, easing: 'easeOutQuart' },
+      y: { duration: 0.8, easing: 'ease' },
+      opacity: { duration: 1.5, easing: 'easeOutQuart' },
+      delay: 1,
+      default: { ease: 'linear' },
+    },
+  },
+}
+
+export const motionExit = {
+  x: 90,
+  opacity: 0,
+  transition: {
+    x: { duration: 0.8, easing: 'ease' },
+    opacity: { duration: 0.8, easing: 'easeOutQuart' },
+  },
+}
