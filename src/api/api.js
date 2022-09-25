e68d4ac2f08a5e27b0a594dbf253da6af3c9b431
alvalenda <banysan@msn.com>
2022-09-25 09:38:35 -0300

    feat: createTodo

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,20 @@
 const defaultUrl = 'http://localhost:3000/todos'
 
-export const api = {
-  getAllTodos: async () => {
+export class Api {
+  static async getAllTodos() {
     const response = await fetch(defaultUrl + '/find-all')
     const allTodos = await response.json()
 
     return allTodos
-  },
+  }
+
+  static async createTodo(todo) {
+    const response = await fetch(defaultUrl + '/create', {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+      body: JSON.stringify(todo),
+    })
+    const newTodo = await response.json()
+    return newTodo
+  }
 }
