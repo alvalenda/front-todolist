ed8e30e70b220f631b8eb1225d1242187cea3e06
alvalenda <banysan@msn.com>
2022-11-04 19:13:10 -0300

    feat: getFullYear

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,10 @@
 import './Footer.css'
 
 export const Footer = () => {
+  const actualYear = new Date().getFullYear()
   return (
     <div className='footer'>
-      <p> 2022&#174; </p>
+      <p> {actualYear}&#174; </p>
       <a href={'http://github.com/alvalenda'} target='_blank' rel='noreferrer'>
         <p>
           alva.code {`<`}
